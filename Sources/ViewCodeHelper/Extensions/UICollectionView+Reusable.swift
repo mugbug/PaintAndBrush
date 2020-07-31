09abd8a3a0f98dfde0cdd8ab068c2cc8e355fe83
mugbug <zaronipedro@outlook.com>
2020-07-30 22:31:08 -0300

    Add reusable helpers for supplementary view

--- Sources/ViewCodeHelper/Extensions/UICollectionView+Reusable.swift
+++ Sources/ViewCodeHelper/Extensions/UICollectionView+Reusable.swift
@@ -9,6 +9,19 @@ public extension UICollectionView {
         return cell
     }
 
+    func dequeueReusableSupplementaryView<T: UICollectionReusableView>(
+        ofKind kind: String,
+        for indexPath: IndexPath
+    ) -> T where T: Reusable {
+        let supplementaryView = dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: T.identifier,
+            for: indexPath
+        )
+        guard let view = supplementaryView as? T else { return T() }
+        return view
+    }
+
     func registerNib<T: UICollectionViewCell>(_: T.Type) where T: Reusable {
         let bundle = Bundle(for: T.self)
         let nib = UINib(nibName: T.identifier, bundle: bundle)
@@ -17,5 +30,9 @@ public extension UICollectionView {
 
     func register<T: UICollectionViewCell>(_: T.Type) where T: Reusable {
         register(T.self, forCellWithReuseIdentifier: T.identifier)
+    }
+
+    func register<T: UICollectionReusableView>(_: T.Type, forSupplementaryViewOfKind kind: String) where T: Reusable {
+        register(T.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.identifier)
     }
 }
